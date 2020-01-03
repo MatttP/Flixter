4e8830d107bb62605b94732127006662f79deab1
Matt Parr <mattt.parr@gmail.com>
2020-01-03 22:05:47 +0000

    started code for student enrolled authentication

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :enrolled_for_course
 
   def show
 
@@ -12,5 +13,10 @@ class LessonsController < ApplicationController
     @current_lesson ||= Lesson.find(params[:id])
   end
 
+  def enrolled_for_course
+    if current_user != current_user.enrolled_in?(@course)
+      redirect_to course_path(current_course)
+    end
+    
 end
 
